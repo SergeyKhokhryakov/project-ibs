10882edcf39237f0b34549df7409fc8d381b58d3
SergeyKhokhryakov <hso@list.ru>
2024-01-29 19:21:02 +0300

    Resolved condition on accepting terms

--- src/test/java/ru/ostrovcy/ibs/base/TestBaseRemote.java
+++ src/test/java/ru/ostrovcy/ibs/base/TestBaseRemote.java
@@ -1,27 +1,28 @@
 package ru.ostrovcy.ibs.base;
 
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import ru.ostrovcy.ibs.config.WebProvider;
 import ru.ostrovcy.ibs.helpers.Attach;
 import ru.ostrovcy.ibs.pages.MainPage;
 
-import java.util.List;
 import java.util.Set;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 
 public class TestBaseRemote {
 
   protected MainPage mainPage = MainPage.getInstance();
   private static String locatorCookies = ".cookies-right";
+  private static String locatorCookiesXPath = "//div[@class=\"cookies js-cookies\"]";
   private static Set<Cookie> cookies;
 
   @BeforeAll
@@ -42,10 +43,11 @@ public class TestBaseRemote {
     Attach.pageSource();
     Attach.browserConsoleLogs();
     Attach.addVideo();
-    WebDriverRunner.getWebDriver().manage().deleteAllCookies();
   }
 
-  public static void acceptTerms(){
-    $(locatorCookies).click();
+  public static void acceptTerms() {
+    if(!$$(By.xpath(locatorCookiesXPath)).isEmpty()){
+      $(locatorCookies).click();
+    };
   }
 }
